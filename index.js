1b9054b8006a9b9d9fe69c389f389898ef054524
Ahmet Korkmaz <ahmetkorkmaz3453@gmail.com>
2022-09-07 22:00:49 +0300

    add linter

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const mjml = require('mjml');
-const bodyParser = require('body-parser');
-const minify = require('html-minifier').minify;
+const express = require('express')
+const mjml = require('mjml')
+const bodyParser = require('body-parser')
+const minify = require('html-minifier').minify
 
-const app = express();
-const port = 8000;
+const app = express()
+const port = 8000
 
-app.use(bodyParser.text());
-app.use(bodyParser.json());
+app.use(bodyParser.text())
+app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
     const beautify = req.query.beautify === 'true'
@@ -18,30 +18,26 @@ app.post('/', (req, res) => {
         let htmlOutput = mjml(req.body, {
             beautify,
             validationLevel,
-        }).html;
+        }).html
 
         if (isMinify) {
-            htmlOutput = minify(htmlOutput);
+            htmlOutput = minify(htmlOutput)
         }
 
         if (req.headers.accept === 'application/json') {
-            res
-                .status(200)
-                .send({
-                    "data": htmlOutput,
-                });
+            res.status(200).send({
+                data: htmlOutput,
+            })
         } else {
-            res.status(200)
-                .send(htmlOutput);
+            res.status(200).send(htmlOutput)
         }
     } catch (e) {
-        res.status(400)
-            .send({
-                "error": e.message,
-            });
+        res.status(400).send({
+            error: e.message,
+        })
     }
-});
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
